4463a94db24a1d7735009f2cf3ec2b8b6ccbe44a
xwwwb <xuwenbo@88.com>
2022-11-26 01:53:46 +0800

    深夜学习C++到高潮🫠🫠

--- CppBase/main.cpp
+++ CppBase/main.cpp
@@ -1,8 +1,29 @@
 #include "main.h"
 #include <QCoreApplication>
+
+void Constructor();
+void Polymorphism();
 int main(int argc, char *argv[]) {
   QCoreApplication a(argc, argv);
+  //  Constructor(); // 构造函数的学习
+  Polymorphism();
+  return 0;
+}
 
+void Constructor() {
+  string notebook = "1. 默认构造函数\n"
+                    "\t如果没有任何构造函数，编译器会自动提供一个，如果写了构造"
+                    "函数，编译器就不会自动提供\n"
+                    "2. 有参构造函数\n"
+                    "\t用的最多的构造函数\n"
+                    "3. 拷贝构造函数\n"
+                    "\t拷贝构造函数可以访问对象的私有成员变量，因为封装只针对类"
+                    "而不针对类的对象\n"
+                    "4. 移动构造函数\n"
+                    "\t没听懂...3.5章节的构造函数\n"
+                    "5. 委托构造函数\n"
+                    "\t构造函数中调用构造函数";
+  cout << notebook;
   Animal ani("xwwwb", 18);
   std::cout << ani << std::endl;
   Animal ani2("xwwwwwb", 20);
@@ -10,5 +31,38 @@ int main(int argc, char *argv[]) {
   std::cout << ani3 << std::endl;
   cout << Animal::animalCount() << endl;
   cout << Animal::cnt2 << endl;
-  return 0;
+
+  // 以下两种写法都是拷贝构造函数
+  Animal ani4(ani2);
+  Animal ani5 = ani3;
+  cout << ani4 << endl;
+  cout << ani5 << endl;
+
+  // 委托构造范例
+  Animal cat(2, "Cat");
+  cout << cat << endl;
+}
+void Polymorphism() {
+  Dog dog("哈士奇", 2);
+
+  cout << dog << endl;
+  dog.getName();
+
+  Dog dog2(4, 2, "边牧");
+  dog2.cry();
+  cout << dog2 << endl;
+
+  // 父类指针或引用指向子类对象
+  // 虚函数重写
+
+  Animal *dog3 = new Dog(2, 10, "柯基");
+  Animal *cat = new Animal("Cat", 2);
+
+  dog3->cry();
+  show(*dog3);
+  show(*cat);
+  cout << endl;
+  dog.limit();
+  delete dog3;
+  delete cat;
 }
--- CppBase/main.h
+++ CppBase/main.h
@@ -7,6 +7,7 @@ using namespace std;
 
 class Animal {
 public:
+  int age;
   // cnt的初始化遵循C++17之前的写法
   static int cnt;
   // cnt2是C++17之后的声明写法
@@ -17,13 +18,35 @@ public:
     cnt2++;
   }
 
+  Animal(int age) : age(age), name("") {
+    cout << "这是一个只初始化age的构造函数" << endl;
+  }
+  Animal(int age, const string &name)
+      : Animal(age) // 委托构造后面不能接初始化
+  {
+    this->name = name;
+    cout << "这里是委托构造";
+  }
+
+  Animal() : age(0), name("Animal") {}
+
+  Animal(const Animal &other) {
+    this->age = other.age;
+    this->name = other.name;
+  }
   friend ostream &operator<<(ostream &, Animal &);
   Animal operator+(Animal);
   static int animalCount() { return cnt; };
+  virtual void cry() const { cout << "I am Animal" << endl; }
+
+  // 可以通过派生类调用这个方法拿到基类私有的name
+  void getName() { cout << name << endl; }
 
 private:
-  int age;
   string name;
+
+protected:
+  int protectVar;
 };
 
 ostream &operator<<(ostream &out, Animal &animal) {
@@ -39,5 +62,31 @@ Animal Animal::operator+(Animal a) {
 // 如果不存在其他的初始化语句，在创建第一个对象时，所有的静态数据都会被初始化为零。
 int Animal::cnt;
 // int Animal::cnt = 0;
+
+class Dog : public Animal {
+  // 使用父类构造函数
+  using Animal::Animal;
+
+public:
+  Dog(int foot, int age, const string &name) : foot(foot), Animal(name, age) {}
+  // 如果派生类在虚函数声明时使用了override描述符，那么该函数必须重载其基类中的同名函数，否则代码将无法通过编译。
+  void cry() const override { cout << "汪汪~" << endl; }
+  void limit() {
+    // Dog身上没age 这里this->age指向了dog的基类中的age
+    // protectVar也是一样
+    cout << this->age << endl;
+    cout << this->protectVar << endl; // 没初始化 不影响
+    //  cout << this->name << endl; // 不可访问
+    // 流输出调用的是父节点的函数 可以访问name
+  }
+
+private:
+  int foot;
+};
+
+// 这里要给cry函数写上const限定符表示是只读函数 因为我在访问的时候
+// 只读的指针只能访问只读的函数
+// 如果两个cry没有const限定 这里的ani是只读的 ani就不能使用cry方法
+void show(const Animal &ani) { ani.cry(); }
 
 #endif // MAIN_H
